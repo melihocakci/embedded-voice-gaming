1b455f4d6b822d526b92d78c25ba70adccadb550
Melih Ocakci <melihocakci@gmail.com>
2022-11-28 21:14:32 +0000

    added some regex

--- speech2text.py
+++ speech2text.py
@@ -1,28 +1,36 @@
 import speech_recognition as sr
+import regex as re
 
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.record(source, duration=3)
+    
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said in English: -  " +
-          r.recognize_google(audio, language="en-US"))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print(
-        "Could not request results from Google Speech Recognition service; {0}".format(e))
-try:
-    print("Google Speech Recognition thinks you said in Turkish: -  " +
-          r.recognize_google(audio, language="tr-TR"))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print(
-        "Could not request results from Google Speech Recognition service; {0}".format(e))
+# # recognize speech using Google Speech Recognition
+# try:
+#     # for testing purposes, we're just using the default API key
+#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+#     # instead of `r.recognize_google(audio)`
+#     print("Google Speech Recognition thinks you said in English: -  " +
+#           r.recognize_google(audio, language="en-US"))
+# except sr.UnknownValueError:
+#     print("Google Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print(
+#         "Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    while True:
+        input("press enter\n")
+        print("Say something!")
+        audio = r.record(source, duration=3)
+
+        try:
+            result = r.recognize_google(audio, language="tr-TR")
+            print("Google Speech Recognition thinks you said in Turkish: -  " + result)
+            
+            num = re.findall('\d+', result)[0]
+            print(num)   
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
